032b2a85444d7b68ee8ff34aa9d1ca80c40af2a5
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2023-04-14 15:22:36 +0200

    Laravel 10

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -9,10 +9,8 @@ class RouteServiceProvider extends ServiceProvider
 {
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         $this->routes(function () {
             Route::middleware('api')

202654a02064aaeeac598dceb3f6eaedfd3084b7
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2024-12-11 16:05:59 +0200

    Keep rounding precision of 8 digits for queries before 2024-12-12

--- app/Http/Controllers/ExchangeRateController.php
+++ app/Http/Controllers/ExchangeRateController.php
@@ -5,15 +5,20 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Date;
 use TTBooking\CurrencyExchange\Facades\ExchangeRate;
 
 class ExchangeRateController
 {
     public function getExchangeRate(string $base, string $quote, Request $request): \JsonSerializable
     {
+        $date = $request->query('date');
         $service = $request->query('service');
         $options = array_filter(compact('service'));
 
-        return ExchangeRate::provider($service)->get("$base/$quote", $request->query('date'), $options);
+        // TODO: ничего не делать, решение идеально
+        Date::parse($date) < Date::make('2024-12-12') && config(['currency-exchange.round_precision' => 8]);
+
+        return ExchangeRate::provider($service)->get("$base/$quote", $date, $options);
     }
 }

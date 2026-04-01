0ece044515d02579fdcd3eb7984e29e6d1b3cb36
Egor Gruzdev <egor.gruzdev@gmail.com>
2026-04-01 08:50:27 +0200

    CORE: добавлен timeout для работы с внешними поставщиками, в случае работы через Guzzle

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,14 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Providers;
 
+use Http\Discovery\Strategy\CommonPsr17ClassesStrategy;
 use Illuminate\Support\ServiceProvider;
+use Http\Discovery\Psr18ClientDiscovery;
+use Http\Discovery\Strategy\DiscoveryStrategy;
+use Psr\Http\Client\ClientInterface;
+use GuzzleHttp\Client as GuzzleClient;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +17,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Регистрируем свою стратегию, которая всегда возвращает Guzzle с таймаутом
+        Psr18ClientDiscovery::prependStrategy(
+            new class implements DiscoveryStrategy {
+                public static function getCandidates($type): array
+                {
+                    if ($type === ClientInterface::class) {
+                        return [
+                            [
+                                'class' => fn() => new GuzzleClient([
+                                    'timeout'         => 3,
+                                    'connect_timeout' => 1,
+                                ]),
+                                'condition' => fn() => class_exists(GuzzleClient::class),
+                            ]
+                        ];
+                    }
+
+                    return CommonPsr17ClassesStrategy::getCandidates($type);
+                }
+            }
+        );
     }
 
     /**
